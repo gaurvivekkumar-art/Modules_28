b66cb82a2209eb0b29e0fee6bf87d0e2088d3f5e
VivekGaur <gaur.vivekkumar@gmail.com>
2026-05-28 11:26:43 +0530

    create the name storgaeaccount

--- storge.tf
+++ storge.tf
@@ -1,5 +1,5 @@
-resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
+resource "azurerm_storage_account" "stm" {
+  name                     = "sera"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
